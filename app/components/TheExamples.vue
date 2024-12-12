235cf2c8fcd8258c3a613c012d6fe37896876a1a
倚窗听雨丿醉无心 <994039348@qq.com>
2024-12-12 19:19:20 +0800

    提交

---
+++ app/components/TheExamples.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { ElMessageBox } from 'element-plus';
+  import { ref } from 'vue';
+
+  const { appContext } = getCurrentInstance()!;
+
+  const visible = ref(false);
+
+  function onClick() {
+    ElMessageBox.prompt('Hello World');
+    // ElMessageBox.prompt('Hello World', appContext);
+  }
+</script>
+
+<template>
+  <div py-10>
+    <el-button @click="visible = true">Show Dialog</el-button>
+
+    <el-dialog
+      v-model="visible"
+    >
+      <el-button @click="onClick">
+        Click me
+      </el-button>
+    </el-dialog>
+  </div>
+</template>
